cc2351258adb64e73bfcd52e4de1bb6f34aeb0bc
Lark-wrc <willowlark@outlook.com>
2015-11-08 14:18:27 -0500

    Trainer 'finished'

--- main.py
+++ main.py
@@ -5,23 +5,24 @@ __author__ = 'John Bucknam & Bill Clark'
 
 # Practice Inputs
 X = np.matrix([[1, 1, 1, 1, 1,
-               1, 0, 0, 0, 0,
-               1, 0, 0, 0, 0,
-               1, 1, 1, 1, 1,
-               1, 0, 0, 0, 1,
-               1, 0, 0, 0, 1,
-               1, 1, 1, 1, 1],
-              [1, 1, 1, 1, 1,
-               1, 0, 0, 0, 0,
-               1, 0, 0, 0, 0,
-               1, 1, 1, 1, 0,
-               0, 0, 0, 0, 1,
-               0, 0, 0, 0, 1,
-               1, 1, 1, 1, 0]])
+                1, 0, 0, 0, 0,
+                1, 0, 0, 0, 0,
+                1, 1, 1, 1, 1,
+                1, 0, 0, 0, 1,
+                1, 0, 0, 0, 1,
+                1, 1, 1, 1, 1],
+               [1, 1, 1, 1, 1,
+                1, 0, 0, 0, 0,
+                1, 0, 0, 0, 0,
+                1, 1, 1, 1, 0,
+                0, 0, 0, 0, 1,
+                0, 0, 0, 0, 1,
+                1, 1, 1, 1, 0]])
 
 # Practice Outputs
 y = np.matrix([[0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]])
+
 
 class NeuralNetwork(object):
     def __init__(self, layer_sizes):
@@ -35,8 +36,9 @@ class NeuralNetwork(object):
         if len(self.hiddenLayerSizes) != 0:
             self.W.append(np.matrix(np.random.randn(self.inputLayerSize, self.hiddenLayerSizes[0])))
             for i in range(len(self.hiddenLayerSizes) - 1):
-                self.W.append(np.matrix(np.random.randn(self.hiddenLayerSizes[i], self.hiddenLayerSizes[i+1])))
-            self.W.append(np.matrix(np.random.randn(self.hiddenLayerSizes[len(self.hiddenLayerSizes) - 1], self.outputLayerSize)))
+                self.W.append(np.matrix(np.random.randn(self.hiddenLayerSizes[i], self.hiddenLayerSizes[i + 1])))
+            self.W.append(
+                np.matrix(np.random.randn(self.hiddenLayerSizes[len(self.hiddenLayerSizes) - 1], self.outputLayerSize)))
         else:
             self.W.append(np.matrix(np.random.randn(self.inputLayerSize, self.outputLayerSize)))
 
@@ -51,30 +53,30 @@ class NeuralNetwork(object):
         for i in range(len(self.W) - 1):
             # Append each A value from the sigmoid(Z)
             self.a.append(self.sigmoid(self.z[i]))
-            self.z.append(np.dot(self.a[i], self.W[i+1]))
+            self.z.append(np.dot(self.a[i], self.W[i + 1]))
         y_hat = self.sigmoid(self.z[len(self.z) - 1])
         return y_hat
 
     # Static Sigmoid Function
     @staticmethod
     def sigmoid(z):
-        return 1/(1+np.exp(-z))
+        return 1 / (1 + np.exp(-z))
 
     # Sigmoid Prime (will provide proof from reference)
     def sigmoid_prime(self, z):
-        return self.sigmoid(z)*(1 - self.sigmoid(z))
+        return self.sigmoid(z) * (1 - self.sigmoid(z))
 
     # Sum of Squares Error Cost Function
     def cost_function(self, X, y):
         y_hat = self.forward(X)
-        return .5 * np.sum(y - y_hat)**2
+        return .5 * np.sum(y - y_hat) ** 2
 
     def cost_function_prime(self, X, y):
         # Defines output as well as sum of weighted inputs (z) and the threshold output (a)
         y_hat = self.forward(X);
         delta = []
 
-        delta.append(np.multiply(-(y-y_hat)), )
+        delta.append(np.multiply(-(y - y_hat)), )
 
 
     # Obtains the different weights in a 1-D matrix
@@ -94,16 +96,18 @@ class NeuralNetwork(object):
         hiddenEnd = self.hiddenLayerSizes[0] * self.inputLayerSize
         self.W[0] = np.reshape(params[hiddenStart:hiddenEnd], (self.inputLayerSizes, self.hiddenLayerSizes[0]))
 
-        for layer in range(1,self.hiddenLayerSizes):
-            hiddenStart = hiddenEnd+1
-            hiddenEnd = self.hiddenLayerSizes[layer] * self.inputLayerSizes[layer-1]
+        for layer in range(1, self.hiddenLayerSizes):
+            hiddenStart = hiddenEnd + 1
+            hiddenEnd = self.hiddenLayerSizes[layer] * self.inputLayerSizes[layer - 1]
             self.W[layer] = np.reshape(params[hiddenStart:hiddenEnd],
-                                       (self.hiddenLayerSizes[layer-1],self.hiddenLayerSizes[layer]))
-        hiddenStart = hiddenEnd+1
-        hiddenEnd = self.hiddenLayerSizes[len(self.hiddenLayerSizes)-1] * self.outputLayerSize
-        self.W[len(W)-1] = np.reshape(params[hiddenStart:hiddenEnd],
-                                   (self.hiddenLayerSizes[len(self.hiddenLayerSizes)-1], self.outputLayerSize))
+                                       (self.hiddenLayerSizes[layer - 1], self.hiddenLayerSizes[layer]))
+        hiddenStart = hiddenEnd + 1
+        hiddenEnd = self.hiddenLayerSizes[len(self.hiddenLayerSizes) - 1] * self.outputLayerSize
+        self.W[len(W) - 1] = np.reshape(params[hiddenStart:hiddenEnd],
+                                        (self.hiddenLayerSizes[len(self.hiddenLayerSizes) - 1], self.outputLayerSize))
 
+
+from scipy import optimize
 # Unfinished Trainer Class
 class Trainer(object):
     def __init__(self, N):
@@ -112,6 +116,12 @@ class Trainer(object):
     def callbackF(self, params):
         self.N.setParams(params)
         self.J.append(self.N.costFunction(self.X, self.Y))
+
+    def costFunctionWrapper(self, params, x, y):
+        self.N.setParams(params)
+        cost = self.N.costFunction(X, y)
+        grad = self.N.computeGradients(X,y)
+        return cost, grad
 
     def train(self, X, y):
         self.X = X
@@ -122,7 +132,12 @@ class Trainer(object):
 
         # Parameters of weights from the Neural Network
         params = self.N.getParams()
-        self.N.setParams(params)
+
+        options = {'maxiter': 200, 'disp': True}
+        results = optimize.minimize(self.costFunctionWrapper, params, jac=True, method='BFGS',
+                                    args=(X, y), options=options, callback=self.callbackF)
+        self.N.setParams(results.x)
+        self.optimizationResults = results
 
 # SCRIPT
 NN = NeuralNetwork((35, 15, 10))
