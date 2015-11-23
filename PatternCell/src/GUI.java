a7e0b6904928ad5f7092dc8560ce0c168214428e
Perka Lab <buck@perka.com>
2015-11-22 19:11:37 -0500

    Updated GUI to have output file

--- PatternCell/src/GUI.java
+++ PatternCell/src/GUI.java
@@ -6,9 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.List;
 import java.util.ArrayList;
 import java.lang.Runtime;
@@ -44,8 +42,15 @@ public class GUI
     setButton.setPreferredSize(new Dimension(160, 40));
     setButton.addActionListener(new ActionListener() {
       @Override
-      public void actionPerformed(ActionEvent e) {
-        grid.toString();
+      public void actionPerformed(ActionEvent e)
+      {
+        try {
+          PrintWriter out = new PrintWriter("../test");
+          out.println(grid.toString());
+          out.close();
+        } catch (FileNotFoundException er) {
+          er.printStackTrace();
+        }
         grid.clear();
       }
     });
